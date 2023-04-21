f7981108f382b6fb4517aafec899e4cd0fbda293
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-04-21 19:24:00 +0430

    change main to compiler

---
+++ src/compiler/Compiler.java
@@ -0,0 +1,9 @@
+package compiler;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Compiler {
+    public static void main(String[] args) {
+
+    }
+}
