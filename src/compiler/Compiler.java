65a3921b60351ba3cc148a8da9e80983c7583372
Alireza Kamarzardi <alirezakzt.cs@gmail.com>
2023-04-21 21:39:38 +0430

    main compiler

--- src/compiler/Compiler.java
+++ src/compiler/Compiler.java
@@ -1,9 +1,26 @@
 package compiler;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Compiler {
-    public static void main(String[] args) {
+import gen.DustLexer;
+import gen.DustParser;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Compiler {
+    public static void main(String[] args) throws IOException {
+        var path = "C:\\Users\\user\\IdeaProjects\\Dust\\sample\\input1.txt";
+        var stream = CharStreams.fromFileName(path);
+        var lexer = new DustLexer(stream);
+        var tokens = new CommonTokenStream(lexer);
+        var parser = new DustParser(tokens);
+        parser.setBuildParseTree(true);
+        var tree = parser.program();
+        var walker = new ParseTreeWalker();
+        var listener = new DustListenerImp();
+        walker.walk(listener, tree);
     }
 }
