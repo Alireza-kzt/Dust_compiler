8ee1d80b89ec5184129a04d896f6afc668f70f32
MohammadBaghbanzadeh <mohammad.baqbanzadeh@gmail.com>
2023-04-22 00:03:24 +0330

    input sample 2 in created

--- src/compiler/Compiler.java
+++ src/compiler/Compiler.java
@@ -12,7 +12,8 @@ import java.nio.file.Paths;
 
 public class Compiler {
     public static void main(String[] args) throws IOException {
-        var path = "C:\\Users\\user\\IdeaProjects\\Dust\\sample\\input1.txt";
+//        var path = "C:\\Users\\user\\IdeaProjects\\Dust\\sample\\input1.txt";
+        var path = "/home/mohammad_bq/IdeaProjects/Dust_compiler/sample/input2.txt";
         var stream = CharStreams.fromFileName(path);
         var lexer = new DustLexer(stream);
         var tokens = new CommonTokenStream(lexer);
