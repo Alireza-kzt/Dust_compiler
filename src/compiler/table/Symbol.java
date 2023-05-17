97a267df77ad24bf4aa090e778b155f1ce7e1a38
MohammadBaghbanzadeh <mohammad.baqbanzadeh@gmail.com>
2023-05-18 02:29:18 +0330

    is_defined variable is added.

--- src/compiler/table/Symbol.java
+++ src/compiler/table/Symbol.java
@@ -4,11 +4,13 @@ public class Symbol extends ISymbol {
     String field;
     String name;
     String type;
+    boolean is_defined;
 
     public Symbol(String field, String name, String type) {
         this.field = field;
         this.name = name;
         this.type = type;
+        this.is_defined = false;
     }
 
     public String getValue() {
@@ -29,6 +31,6 @@ public class Symbol extends ISymbol {
 
     @Override
     public void print() {
-        System.out.println("Key: " + getValue() + " | Value : " + parentType() + "(name: " + name + "), (Type: " + type + ")");
+        System.out.println("Key: " + getValue() + " | Value : " + parentType() + "(name: " + name + "), (Type: " + type + "), isDefined: " + is_defined);
     }
 }
