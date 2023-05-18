3fd4326d2318c6ae73a2d0ca8e6713a5207c938d
MohammadBaghbanzadeh <mohammad.baqbanzadeh@gmail.com>
2023-05-18 07:40:44 +0330

    assignment Symbol is deleted.

--- src/compiler/table/IScope.java
+++ src/compiler/table/IScope.java
@@ -1,6 +1,5 @@
 package compiler.table;
 
-import java.util.List;
 import java.util.Stack;
 
 public abstract class IScope extends ISymbol {
@@ -8,22 +7,10 @@ public abstract class IScope extends ISymbol {
     public String name;
     public int line;
 
-    public Symbol assignment(String name) {
-        for (var scope : scopes) {
-            if (scope instanceof Symbol) {
-                if (((Symbol) scope).name == name) {
-                    ((Symbol) scope).is_defined = true;
-                }
-            }
-        }
-
-        return null;
-    }
-
     public IScope(String name, int line) {
         this.name = name;
         this.line = line;
-        this.scopes = new Stack();
+        this.scopes = new Stack<>();
     }
 
     public IScope add(IScope scope) {
